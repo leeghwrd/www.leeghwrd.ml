e15e38c39d19e4e0be63d584af9d2f6c2b669cf8
Lee Howard <lee.develops@icloud.com>
2018-11-20 16:03:36 -0800

    button link to pages.

--- src/components/underConstruction.js
+++ src/components/underConstruction.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const UnderConstruction = () => (
     <section className="section">
@@ -19,6 +20,38 @@ const UnderConstruction = () => (
   >
     Thank You for visiting! Have a look around, and please come back soon.
   </p>
+
+  <div style={{
+      display: `flex`,
+      margin: `0 auto`,
+      padding: 0,
+      justifyContent: `center`,
+    }}
+  >
+  <Link to="/about"><button style={{
+      backgroundColor: `#d3d3d3`,
+      color: `#111`,
+      width: 100,
+      height: `auto`,
+      margin: `0 20px`,
+      padding: 10,
+      border: `1px solid #f3f3f3`,
+      borderRadius: 40
+    }}
+  >About Me</button></Link>
+  
+  <Link to="/articles"><button style={{
+      backgroundColor: `#d3d3d3`,
+      color: `#111`,
+      width: 100,
+      height: `auto`,
+      margin: `0 20px`,
+      padding: 10,
+      border: `1px solid #f3f3f3`,
+      borderRadius: 40
+    }}
+  >Artciles</button></Link>
+  </div>
   </section>
 )
 
