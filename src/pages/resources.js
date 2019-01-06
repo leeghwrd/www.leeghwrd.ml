cda4b8c315179e03902ce9d35eea68a2529252b8
Lee Howard <lee.develops@icloud.com>
2019-01-05 23:55:54 -0800

    convert all links from gatsby Link to a href tags.

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Link } from 'gatsby'
 
 const Resources = () => (
   <Layout>
@@ -8,16 +7,16 @@ const Resources = () => (
    <h1>Resources</h1>
       <h2>Tools</h2>
         <ul>
-          <li><Link to="https://www.google.com">Google</Link> - Everyone needs to search for something from time to time.</li>
-          <li><Link to="https://code.visualstudio.com">VSCode</Link> - Great code editor, I use this for web development on mac.</li>
+          <li><a href="https://www.google.com">Google</a> - Everyone needs to search for something from time to time.</li>
+          <li><a href="https://code.visualstudio.com">VSCode</a> - Great code editor, I use this for web development on mac.</li>
         </ul>
    <h2>Music</h2>
    <ul>
-     <li><Link to="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=video&cd=3&cad=rja&uact=8&ved=0ahUKEwj-05XvqNjfAhXLs1QKHTGrBFMQtwIIMDAC&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DZKFwQFBwQFU&usg=AOvVaw1M5izB06eTUvBqPALCUU4z">50 Famous Pieces Of Classical Music</Link> - Helps me concentrate.</li>
+     <li><a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=video&cd=3&cad=rja&uact=8&ved=0ahUKEwj-05XvqNjfAhXLs1QKHTGrBFMQtwIIMDAC&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DZKFwQFBwQFU&usg=AOvVaw1M5izB06eTUvBqPALCUU4z">50 Famous Pieces Of Classical Music</a> - Helps me concentrate.</li>
    </ul>
    <h2>Books</h2>
    <ul>
-   <li><Link to="https://docs.swift.org/swift-book/TheSwiftProgrammingLanguageSwift42.epub">Swift programming language</Link> - Apple's awesome programming language.</li>
+   <li><a href="https://docs.swift.org/swift-book/TheSwiftProgrammingLanguageSwift42.epub">Swift programming language</a> - Apple's awesome programming language.</li>
    </ul>
    </div>
   </Layout>
