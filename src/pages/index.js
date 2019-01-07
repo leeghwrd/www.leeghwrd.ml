bb5cc10cb3bbeabbbe6661602acd4b369a52a2d1
Lee Howard <lee.develops@icloud.com>
2019-01-06 16:24:30 -0800

    stripped down duplicate of articles.js using HomeIndex also for better semantic meaning.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,59 @@
-import React from 'react'
-// import { Link } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
 
-import Layout from '../components/layout'
-import UnderConstruction from '../components/underConstruction';
-// import Image from '../components/image'
+export default function HomeIndex({ data }) {
+  
+  const { edges: posts } = data.allMarkdownRemark
+  return (
+    <Layout>
+     
+      <section className="list">
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }) => (
+              <Link to={post.frontmatter.path} key={post.id} className="post">
+        <div className="post-thumbnail">
+          <img src={post.frontmatter.thumbnail.childImageSharp.fluid.src} alt="post"></img>
+        </div>
+          <div className="post-title">{post.frontmatter.title}</div>
+  <span className="post-date"><time>{post.frontmatter.date}</time></span>
+              </Link>
+        ))}
+      </section>
+    </Layout>
+  )
+}
 
-const IndexPage = () => (
-  <Layout>
-    <UnderConstruction />
-  </Layout>
-)
+HomeIndex.propTypes = {
+  data: PropTypes.object,
+}
 
-export default IndexPage
+export const pageQuery = graphql`
+  query HomeIndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      totalCount
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            path
+            date(formatString: "MMMM DD, YYYY")
+            yearSlug
+            thumbnail {
+              childImageSharp {
+									id
+                fluid {                 
+                  src                  
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
