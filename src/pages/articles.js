d5850f9b08236aa799753ba2785ee2bbece70e6e
Lee Howard <lee.develops@icloud.com>
2019-03-08 10:43:51 -0800

    removed posts filter for more transparent implentation. removed  SearchQuery import.

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -4,8 +4,6 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-// import SearchQuery from "../components/searchQuery";
-
 export default class ArticleIndex extends Component {
  
   render() {
@@ -16,20 +14,19 @@ export default class ArticleIndex extends Component {
   return (
     <Layout>
 
-      {/* <SearchQuery /> */}
      <h1>Articles</h1>
       <section className="list">
-      {posts
-        .filter(post => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => (
-              <Link to={post.frontmatter.path} key={post.id} className="post">
+      
+      {posts.map((post) => (
+              <Link to={post.node.frontmatter.path} key={post.id} className="post">
         <div className="post-thumbnail">
-          <img src={post.frontmatter.thumbnail.childImageSharp.fluid.src} alt="post"></img>
+          <img src={post.node.frontmatter.thumbnail.childImageSharp.fluid.src} alt="post"></img>
         </div>
-          <div className="post-title">{post.frontmatter.title}</div>
-        <span className="post-date"><time>{post.frontmatter.date}</time></span>
+          <div className="post-title">{post.node.frontmatter.title}</div>
+        <span className="post-date"><time>{post.node.frontmatter.date}</time></span>
               </Link>
         ))}
+
       </section>
     </Layout>
   )
