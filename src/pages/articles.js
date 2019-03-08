69e862e88e03949446e2c7c52363b5e8563375e6
Lee Howard <lee.develops@icloud.com>
2019-03-08 12:11:53 -0800

    filterPosts then map using bind search state from input value. wrap Article component inside Link.

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,27 +1,51 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { graphql } from "gatsby"
+import { Link , graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Article from "../components/article";
 
 export default class ArticleIndex extends React.Component {
- 
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    }
+  }
+
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0,20)});
+  }
+
   render() {
     const { data } = this.props
 
     const { edges: posts } = data.allMarkdownRemark
     
+    let filteredPosts = posts.filter(
+      (post)=> {
+        return post.node.frontmatter.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      } 
+    );
+
   return (
+
     <Layout>
 
      <h1>Articles</h1>
       <section className="list">
-      {posts.map((post) => {
-           return <Article post={post} key={post.node.id}/>   
+      <input type="text"
+      placeholder="Search Articles"
+      value={this.state.search}
+      onChange={this.updateSearch.bind(this)}
+      />
+      {filteredPosts.map((post) => {
+           return <Link to={post.node.frontmatter.path} key={post.node.id}><Article post={post} key={post.node.id}/> </Link>
+  
         })
       }
       </section>
+      
     </Layout>
   )
         }
