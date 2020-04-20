0b1db43d71d0cf868be78e54917aa385b9a3c7e7
leeghwrd <leeghwrd@gmail.com>
2020-04-20 00:25:31 -0700

    Install stylelint-order and run fix
    - Add rules to .stylelintrc.js
    
    - npm run lint_scss:fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,13 @@ module.exports = {
     extends: [
         "stylelint-config-recommended-scss",
         "stylelint-prettier/recommended"
-    ]
+    ],
+    plugins: [ "stylelint-order" ],
+    rules: {
+		"order/order": [
+			"custom-properties",
+			"declarations"
+		],
+		"order/properties-alphabetical-order": true
+	}
 }
