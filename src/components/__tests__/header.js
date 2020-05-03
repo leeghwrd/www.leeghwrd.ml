8c9c627cba13478333d04e6aefa0b9b5a0c2eb0d
leeghwrd <leeghwrd@gmail.com>
2020-05-02 17:23:18 -0700

    Test links in header
    - Links section
    
    - Extra section

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -11,4 +11,55 @@ describe(`Header`, () => {
 
     expect(title).toBeInTheDocument();
   });
+
+  it(`renders About me link`, () => {
+    const aboutMeTxt = `About me`;
+    const { getByText } = render(<Header />);
+
+    const aboutMe = getByText(aboutMeTxt);
+
+    expect(aboutMe).toBeInTheDocument();
+  });
+
+  it(`renders Articles link`, () => {
+    const articlesTxt = `Articles`;
+    const { getByText } = render(<Header />);
+
+    const articles = getByText(articlesTxt);
+
+    expect(articles).toBeInTheDocument();
+  });
+
+  it(`renders Resources link`, () => {
+    const resourcesTxt = `Articles`;
+    const { getByText } = render(<Header />);
+
+    const resources = getByText(resourcesTxt);
+
+    expect(resources).toBeInTheDocument();
+  });
+
+  it(`renders Github link`, () => {
+    const { getByLabelText } = render(<Header />);
+
+    const github = getByLabelText("github");
+
+    expect(github).toBeInTheDocument();
+  });
+
+  it(`renders Twitter link`, () => {
+    const { getByLabelText } = render(<Header />);
+
+    const twitter = getByLabelText("twitter");
+
+    expect(twitter).toBeInTheDocument();
+  });
+
+  it(`renders LinkedIn link`, () => {
+    const { getByLabelText } = render(<Header />);
+
+    const linkedIn = getByLabelText("linkedin");
+
+    expect(linkedIn).toBeInTheDocument();
+  });
 });
